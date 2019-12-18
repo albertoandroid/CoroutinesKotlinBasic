3aa59c8ec78e2fc1f433706915e22f1ec0642932
Alberto <info@androiddesdecero.com>
2019-12-18 16:48:26 +0100

    2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,17 @@
 fun main(args: Array<String>) {
-
+    blokingExample()
 }
+
+fun longTaskWithMessage(message: String){
+    Thread.sleep(4000)
+    println(message + Thread.currentThread().name)
+}
+
+fun blokingExample(){
+    println("Tarea1 " + Thread.currentThread().name)
+    longTaskWithMessage("Tarea2 ")
+    println("Tarea3 " + Thread.currentThread().name)
+}
+
+
 
