b131588f8d62dd8090bd729edf18371c9b62a31f
Alberto <info@androiddesdecero.com>
2019-12-18 17:07:54 +0100

    3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlinx.coroutines.delay
+
 fun main(args: Array<String>) {
     blokingExample()
 }
@@ -9,8 +11,14 @@ fun longTaskWithMessage(message: String){
 
 fun blokingExample(){
     println("Tarea1 " + Thread.currentThread().name)
-    longTaskWithMessage("Tarea2 ")
+    //longTaskWithMessage("Tarea2 ")
+    //delayCoroutine("tarea2")
     println("Tarea3 " + Thread.currentThread().name)
+}
+
+suspend fun delayCoroutine(message: String){
+    delay(timeMillis = 4000)
+    println(message + Thread.currentThread().name)
 }
 
 
