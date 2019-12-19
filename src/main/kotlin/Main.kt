ac407be2260c2592fc79a3e05a5d0b02e3483257
Alberto <info@androiddesdecero.com>
2019-12-19 10:07:16 +0100

    3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
-    blokingExample()
+    dispatcher()
+    //blokingExample()
 }
 
 fun longTaskWithMessage(message: String){
@@ -19,6 +23,27 @@ fun blokingExample(){
 suspend fun delayCoroutine(message: String){
     delay(timeMillis = 4000)
     println(message + Thread.currentThread().name)
+}
+
+fun dispatcher(){
+    runBlocking {
+        println("Hilo en el que se ejecuta 1: ${Thread.currentThread().name}")
+    }
+    runBlocking(Dispatchers.Unconfined) {
+        println("Hilo en el que se ejecuta 2: ${Thread.currentThread().name}")
+    }
+    runBlocking(Dispatchers.Default) {
+        println("Hilo en el que se ejecuta 3: ${Thread.currentThread().name}")
+    }
+    runBlocking(Dispatchers.IO) {
+        println("Hilo en el que se ejecuta 4: ${Thread.currentThread().name}")
+    }
+    runBlocking(newSingleThreadContext("MiHilo")) {
+        println("Hilo en el que se ejecuta 5: ${Thread.currentThread().name}")
+    }
+    //runBlocking(Dispatchers.Main) {
+    //    println("Hilo en el que se ejecuta 6: ${Thread.currentThread().name}")
+    //}
 }
 
 
